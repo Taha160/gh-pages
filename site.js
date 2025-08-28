719f3827f07efd3ff893cb6612ff50df8bc63be5
Taha160 <tcvhr@hotmail.com>
2025-08-28 16:05:19 +0300

    Dil Guncellemesi

--- site.js
+++ site.js
@@ -3,3 +3,23 @@ function scrollToSection(hedefalan) {
       behavior: "smooth"
     });
   }
+
+function setLanguage(lang) {
+  const elements = document.querySelectorAll('[data-tr]');
+
+  elements.forEach(el => {
+    const text = el.getAttribute(`data-${lang}`);
+    if (text) el.textContent = text;
+  });
+}
+
+
+document.querySelectorAll('.sidebar button')[3].addEventListener('click', () => {
+  setLanguage('tr');
+});
+
+
+document.querySelectorAll('.sidebar button')[4].addEventListener('click', () => {
+  setLanguage('eng');
+});
+
